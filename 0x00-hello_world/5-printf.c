e9082717ac36f46f6273c3a5455ddf3499440363
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-20 19:43:22 -0700

    fr

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stodio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,6 +6,6 @@
  */
 int main (void)
 {	
-	puts("Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }	
