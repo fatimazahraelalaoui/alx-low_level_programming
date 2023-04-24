b9ec9df34fd86a12abf19e5a4124e1a0c76c5ae3
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-24 14:58:05 -0700

    fr

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	("size of char : %d byte(s)\n", sizeof(char));
+	("size of int : %d byte(s)\n", sizeof(int));
+	("size of long int : %d byte(s)\n", sizeof(long int));
+	("size of long long int : %d byte(s)\n", sizeof(long long int));
+	("size of float : %d byte(s)\n", sizeof(float));
+	return (0);
