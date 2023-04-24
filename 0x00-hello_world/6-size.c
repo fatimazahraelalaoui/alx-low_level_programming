0bb7cb0b7980bbce8c0f85ed558a3acdaf1906f1
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-24 15:10:06 -0700

    fr

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	("size of a  char: %d byte(s)\n", sizeof(char));
-	("size of an int: %d byte(s)\n", sizeof(int));
-	("size of a long int: %d byte(s)\n", sizeof(long int));
-	("size of a long long int: %d byte(s)\n", sizeof(long long int));
-	("size of a float: %d byte(s)\n", sizeof(float));
+	("Size of a  char: %d byte(s)\n", sizeof(char));
+	("Size of an int: %d byte(s)\n", sizeof(int));
+	("Size of a long int: %d byte(s)\n", sizeof(long int));
+	("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
