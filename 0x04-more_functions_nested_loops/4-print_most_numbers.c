8fa42b291954cd9adac81390eacd69f1cfa783f3
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-27 14:21:51 -0700

    fr

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,11 @@
  */
 void print_most_numbers(void)
 {
-	int x;
+	int x = 0;
 
-	for (x = 0; x <= 9; x++)
+	for (; x <= 9; x++)
 	{
-	if ( x == 2 || x == 4)
+	if (x == 2 || x == 4)
 	{
 	continue;
 	}
@@ -21,4 +21,5 @@ void print_most_numbers(void)
 	}
 	}
 	_putchar('\n');
-}	
+}
+	
