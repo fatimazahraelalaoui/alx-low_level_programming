955f29463ba60add4acfaffc57dbeb143ad8909d
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-27 07:54:31 -0700

    fr

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
  * Return: 1 for uppercase or 0 for otherwise
  */
 
-int _isupper(int c);
+int _isupper(int c)
 
 {
-	if (c >= 65 && c <= 90)
 
-	{
+	if (c >= 65 && c <= 90)
+        {
 	return (1);
 	}
 	return (0);
