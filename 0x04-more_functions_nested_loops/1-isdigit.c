de586e491a8d103c38127fd0a565977da49efd76
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-27 12:28:54 -0700

    fr

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,9 @@
 /**
  * _isdigit - Check if the caracter is digit
  * @c: The caracter to be checked
- * Return: 1 for the caracter is digit or 0 for else
+ * Return: 1 for a caracter is digit or 0 for else
  */
-
 int _isdigit(int c)
-
 {
 
 	if (c >= 48 && c <= 57)
