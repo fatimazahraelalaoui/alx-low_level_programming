519c32a58079c65c08c5f2edaedfa235c8771cd8
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-27 07:07:24 -0700

    fr

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * -isupper - Checks for upper caracter
+ *  @x: The caracter to be checked
+ *  Return: 1 for uppercase or 0 for otherwise
+ */
+
+int _isupper(int c);
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
