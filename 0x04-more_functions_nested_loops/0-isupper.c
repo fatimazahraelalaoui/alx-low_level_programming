84ac529ba9c71e3d29b22106d9b45d6f75ba7ac0
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-27 07:43:06 -0700

    fr

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,15 @@
 
 /**
  * -isupper - Checks for upper caracter
- *  @c: The caracter to be checked
- *  Return: 1 for uppercase or 0 for otherwise
+ * @c: The caracter to be checked
+ * Return: 1 for uppercase or 0 for otherwise
  */
 
 int _isupper(int c);
 
 {
 	if (c >= 65 && c <= 90)
+
 	{
 	return (1);
 	}
