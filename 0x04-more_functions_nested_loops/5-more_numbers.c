8e22944236b43946489585375cc1143f76c5c64a
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-04-28 18:53:33 -0700

    fr

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
         int x, y;
 
-        for (x = 0; x = 14; x++)
+        for (x == 0; x == 14; x++)
         {
-                for (y = 0; y = 14; y++)
+                for (y == 0; y == 14; y++)
                 {
                         if (y > 0)
                         {
