0e8d7fdc0fc87fb9fe7921add1d68455434808af
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-05-02 17:30:06 -0700

    fr

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - Takes a pointer to an int and updates
  * the value it points to 98
- * @n: An interger input ponter 
+ * @n : An interger input pointer
  * Return: nothing
  */
 
