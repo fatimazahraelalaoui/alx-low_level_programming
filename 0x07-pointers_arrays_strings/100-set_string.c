4c7350f38f1e830a6222d3cd20e4d0b0d77bdf7e
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-05-08 16:16:04 -0700

    fr

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s : Pointer to pointer
+ * @to : Char to point
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
