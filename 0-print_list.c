cdd49c6d63dcc3d4f7b95941f43f00ae1cfd6688
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-06-01 15:26:19 -0700

    fr

--- 0-print_list.c
+++ 0-print_list.c
@@ -9,17 +9,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+ size_t n = 0;
 
-	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		n++;
-	}
-	return (n);
+ while (h)
+ {
+ if (!h->str)
+ printf("[0] (nil)\n");
+ else
+ printf("[%u] %s\n", h->len, h->str);
+ h = h->next;
+ n++;
+ }
+
+ return (n);
 }
 
