9c88101efec6292b900bc9e54d3b5643db3dc0db
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-06-02 13:02:12 -0700

    fr

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,32 +13,31 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *nst;
 	list_t *t = *head;
-	unsigned int len;
+	unsigned int len = 0;
 
 	while (str[len])
 		len++;
-	
+
 	nst = malloc(sizeof(list_t));
-	if(!nst)
+	if (!nst)
 		return (NULL);
 
 	nst->str = strdup(str);
 	nst->len = len;
 	nst->next = NULL;
 
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = nst;
 		return (nst);
 	}
+
 	while (t->next)
 		t = t->next;
 
 	t->next = nst;
 
-	return(nst);
+	return (nst);
 }
-		
 
-
-
+	
