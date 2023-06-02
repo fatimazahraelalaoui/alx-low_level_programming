287ee63304d21c7a5218c9c6f9e20b21a8e524e5
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-06-01 18:22:17 -0700

    fr

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * print_before - prints a sentence before the main
+ * function is executed
+ */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
