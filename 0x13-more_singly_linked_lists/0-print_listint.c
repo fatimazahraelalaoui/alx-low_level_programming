34ebe352bf99bac093e83dfd5e74dedcaabf9dd6
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-06-05 16:18:53 -0700

    fr

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,16 +5,17 @@
  * @h: linked list to print
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		n++;
+		num++;
 		h = h->next;
 	}
-	return (n);
+
+	return (num);
 }
+
