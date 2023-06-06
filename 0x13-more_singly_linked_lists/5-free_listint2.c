424e9eb98e86cd6f01add44e12563ad4ba3ce14a
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-06-06 15:11:02 -0700

    fr

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,23 @@
 #include "lists.h"
 
 /**
- * free-listint2 - frees a linkes list
- * @head: pointer to point
+ * free_listint2 - frees a linked list
+ * @head: pointer to the list to be freed
  */
-
 void free_listint2(listint_t **head)
 {
 	listint_t *t;
 
 	if (head == NULL)
 		return;
+
 	while (*head)
 	{
 		t = (*head)->next;
 		free(*head);
 		*head = t;
 	}
+
 	*head = NULL;
 }
+
