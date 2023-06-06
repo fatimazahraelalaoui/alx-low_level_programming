b2b777d6c6c8d8dbe0db514bfbb95d54f71a25c6
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-06-05 17:16:28 -0700

    fr

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,14 +15,14 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
 
-			for (j = 0; j < i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
